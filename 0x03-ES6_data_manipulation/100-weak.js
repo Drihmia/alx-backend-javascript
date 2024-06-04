16fe664c6bca22cb9172eadf1c3b639f8287e651
Drihmia <drihmia.redouane@gmail.com>
2024-06-04 23:41:54 +0100

    [UPDATE] task 100: handle the case where the argument passed to queryAPI function are not an object - like, since the WeakMap accept only object as its key

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,7 @@
 const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
+  if (typeof endpoint !== 'object') return;
   if (weakMap.has(endpoint)) {
     if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
