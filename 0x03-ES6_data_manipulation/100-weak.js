d2463742f6c41b4faa14afffb8411c08bf132559
Drihmia <drihmia.redouane@gmail.com>
2024-06-05 00:08:02 +0100

    [UPDATE] task 100: testing sth

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,13 @@
 const weakMap = new WeakMap();
 
-function queryAPI(endpoint) {
+export function queryAPI(endpoint) {
   if (typeof endpoint !== 'object') return;
+
   if (weakMap.has(endpoint)) {
-    if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+    if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
   } else {
     weakMap.set(endpoint, 1);
   }
 }
-export { weakMap, queryAPI };
+export { weakMap };
