d10bde504cc4a9ede4c719a0dcac39f3dbb2684e
Drihmia <drihmia.redouane@gmail.com>
2024-06-05 19:42:20 +0100

    init commit: task0 of 0x04-TypeScript

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,23 @@
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+}
+
+const studentOne = {
+	firstName: 'Redouane',
+	lastName: 'Drihmia',
+	age: 30,
+	location: 'MA',
+};
+
+const studentTwo = {
+	firstName: 'Red',
+	lastName: 'Dri',
+	age: 29,
+	location: 'MA',
+};
+
+const studentsList = [studentOne, studentOne];
+export default  studentsList;
