49a17acaa7ec3ef1b282bbc94bc0c4047f0b6cb6
Drihmia <drihmia.redouane@gmail.com>
2024-07-25 19:10:14 +0100

    fix: imporved style and applied best practice

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -8,9 +8,9 @@
 
 const calculateNumber = (type, a, b) => {
   const divide = (argOne, argTwo) => {
-      if (!Math.round(b)) return 'Error';
-      return (Math.round(argOne) / Math.round(argTwo));
-    }
+    if (!Math.round(b)) return 'Error';
+    return (Math.round(argOne) / Math.round(argTwo));
+  };
   const operationObject = {
     SUM: Math.round(a) + Math.round(b),
     SUBTRACT: Math.round(a) - Math.round(b),
