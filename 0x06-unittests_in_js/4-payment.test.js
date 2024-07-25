5fb2330af427a6cf314a04b26118f00fcef184a6
Drihmia <drihmia.redouane@gmail.com>
2024-07-25 22:46:06 +0100

    feat: add files for task 4

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,41 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { sendPaymentRequestToApi } = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberSub;
+  let logSpy;
+  const a = 100;
+  const b = 20;
+
+  beforeEach(() => {
+    calculateNumberSub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+    // Create a spy for Utils.calculateNumber
+    logSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    // Restore the original method
+    sinon.restore();
+  });
+
+  it('when calling sendPaymentRequestToApi, should call calculateNumber once', () => {
+    // CALL THE FUNCTION ONCE
+    sendPaymentRequestToApi(a, b);
+
+    // CHECK IF THE FUNCTION WAS CALLED ONCE
+    expect(calculateNumberSub.called).to.be.true;
+    expect(logSpy.called).to.be.true;
+  });
+
+  it('when calling sendPaymentRequestToApi, should call calculateNumber with the args : "SUM" and a, b', () => {
+    // CALL THE FUNCTION ONCE
+    sendPaymentRequestToApi(a, b);
+
+    // CHECK IF THE FUNCTION WAS CALLED WITH THE RIGHT ARGUMENTS
+    expect(calculateNumberSub.calledOnceWithExactly('SUM', a, b)).to.equal(true);
+    expect(logSpy.calledOnceWithExactly('The total is: 10')).to.equal(true);
+  });
+});
