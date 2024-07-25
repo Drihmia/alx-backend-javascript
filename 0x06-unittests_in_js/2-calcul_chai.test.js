70690e05ab349f12048258b40d4f18530452d6ff
Drihmia <drihmia.redouane@gmail.com>
2024-07-25 19:33:49 +0100

    t

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,208 +1,208 @@
-const expect = require('chai').expect;
-const calculateNumber = require('./1-calcul.js');
+const { expect } = require('chai');
+const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
-  describe('SUBTRACT', () => {
-    it('SUBTRACT: sum of two positive int', () => {
+  describe('sUBTRACT', () => {
+    it('sUBTRACT: sum of two positive int', () => {
       expect(calculateNumber('SUBTRACT', 2, 5)).to.equal(-3);
     });
-    it('SUBTRACT: subtract between float un integer', () => {
+    it('sUBTRACT: subtract between float un integer', () => {
       expect(calculateNumber('SUBTRACT', 2, 4.6)).to.equal(-3);
     });
-    it('SUBTRACT: subtract between two positive float with round half up', () => {
+    it('sUBTRACT: subtract between two positive float with round half up', () => {
       expect(calculateNumber('SUBTRACT', 2.5, 4.6)).to.equal(-2);
     });
-    it('SUBTRACT: subtract between two positive float with round half down', () => {
+    it('sUBTRACT: subtract between two positive float with round half down', () => {
       expect(calculateNumber('SUBTRACT', 2.3, 4.4)).to.equal(-2);
     });
-    it('SUBTRACT: subtract between two positive float with round half down and other up', () => {
+    it('sUBTRACT: subtract between two positive float with round half down and other up', () => {
       expect(calculateNumber('SUBTRACT', 2.3, 4.7)).to.equal(-3);
     });
-    it('SUBTRACT: subtract between integer, positive and negative', () => {
+    it('sUBTRACT: subtract between integer, positive and negative', () => {
       expect(calculateNumber('SUBTRACT', 2, -4)).to.equal(6);
     });
-    it('SUBTRACT: subtract between two negative int', () => {
+    it('sUBTRACT: subtract between two negative int', () => {
       expect(calculateNumber('SUBTRACT', -2, -4)).to.equal(2);
     });
-    it('SUBTRACT: subtract between two negative float with round half up', () => {
+    it('sUBTRACT: subtract between two negative float with round half up', () => {
       expect(calculateNumber('SUBTRACT', -2.5, -4.6)).to.equal(3);
     });
-    it('SUBTRACT: subtract between two negative float with round half down', () => {
+    it('sUBTRACT: subtract between two negative float with round half down', () => {
       expect(calculateNumber('SUBTRACT', -2.3, -4.4)).to.equal(2);
     });
-    it('SUBTRACT: subtract between two negative float with round half down and other up', () => {
+    it('sUBTRACT: subtract between two negative float with round half down and other up', () => {
       expect(calculateNumber('SUBTRACT', -2.3, -4.7)).to.equal(3);
     });
-    it('SUBTRACT: first parameters is a string', () => {
+    it('sUBTRACT: first parameters is a string', () => {
       expect(calculateNumber('SUBTRACT', '2', 4)).to.equal(-2);
     });
-    it('SUBTRACT: second parameters is a string', () => {
+    it('sUBTRACT: second parameters is a string', () => {
       expect(calculateNumber('SUBTRACT', 2, '4')).to.equal(-2);
     });
-    it('SUBTRACT: both parameters are strings', () => {
+    it('sUBTRACT: both parameters are strings', () => {
       expect(calculateNumber('SUBTRACT', '2', '4')).to.equal(-2);
     });
-    it('SUBTRACT: both parameters are strings, one of them in float format', () => {
+    it('sUBTRACT: both parameters are strings, one of them in float format', () => {
       expect(calculateNumber('SUBTRACT', '2', '4.6')).to.equal(-3);
     });
-    it('SUBTRACT: both parameters are strings in float format', () => {
+    it('sUBTRACT: both parameters are strings in float format', () => {
       expect(calculateNumber('SUBTRACT', '2.5', '4.6')).to.equal(-2);
     });
-    it('SUBTRACT: one of parameters is missing', () => {
+    it('sUBTRACT: one of parameters is missing', () => {
       expect(calculateNumber('SUBTRACT', 2)).to.be.NaN;
     });
-    it('SUBTRACT: both parameters is missing', () => {
-      expect(calculateNumber('SUBTRACT', )).to.be.NaN;
+    it('sUBTRACT: both parameters is missing', () => {
+      expect(calculateNumber('SUBTRACT')).to.be.NaN;
     });
-    it('SUBTRACT: one of parameters is NaN', () => {
+    it('sUBTRACT: one of parameters is NaN', () => {
       expect(calculateNumber('SUBTRACT', NaN, 4)).to.be.NaN;
     });
-    it('SUBTRACT: both parameters is NaN', () => {
+    it('sUBTRACT: both parameters is NaN', () => {
       expect(calculateNumber('SUBTRACT', NaN, NaN)).to.be.NaN;
     });
-    it('SUBTRACT: one of parameters is Infinity', () => {
+    it('sUBTRACT: one of parameters is Infinity', () => {
       expect(calculateNumber('SUBTRACT', Infinity, 4)).to.equal(Infinity);
     });
-    it('SUBTRACT: both parameters is Infinity', () => {
+    it('sUBTRACT: both parameters is Infinity', () => {
       expect(calculateNumber('SUBTRACT', Infinity, Infinity)).to.be.NaN;
     });
   });
-  describe('SUM', () => {
-    it('SUM: sum of two positive int', () => {
+  describe('sUM', () => {
+    it('sUM: sum of two positive int', () => {
       expect(calculateNumber('SUM', 2, 5)).to.equal(7);
     });
-    it('SUM: sum of float un integer', () => {
+    it('sUM: sum of float un integer', () => {
       expect(calculateNumber('SUM', 2, 4.6)).to.equal(7);
     });
-    it('SUM: sum of two positive float with round half up', () => {
+    it('sUM: sum of two positive float with round half up', () => {
       expect(calculateNumber('SUM', 2.5, 4.6)).to.equal(8);
     });
-    it('SUM: sum of two positive float with round half down', () => {
+    it('sUM: sum of two positive float with round half down', () => {
       expect(calculateNumber('SUM', 2.3, 4.4)).to.equal(6);
     });
-    it('SUM: sum of two positive float with round half down and other up', () => {
+    it('sUM: sum of two positive float with round half down and other up', () => {
       expect(calculateNumber('SUM', 2.3, 4.7)).to.equal(7);
     });
-    it('SUM: sum of integer, positive and negative', () => {
+    it('sUM: sum of integer, positive and negative', () => {
       expect(calculateNumber('SUM', 2, -4)).to.equal(-2);
     });
-    it('SUM: sum of two negative int', () => {
+    it('sUM: sum of two negative int', () => {
       expect(calculateNumber('SUM', -2, -4)).to.equal(-6);
     });
-    it('SUM: sum of two negative float with round half up', () => {
+    it('sUM: sum of two negative float with round half up', () => {
       expect(calculateNumber('SUM', -2.5, -4.6)).to.equal(-7);
     });
-    it('SUM: sum of two negative float with round half down', () => {
+    it('sUM: sum of two negative float with round half down', () => {
       expect(calculateNumber('SUM', -2.3, -4.4)).to.equal(-6);
     });
-    it('SUM: sum of two negative float with round half down and other up', () => {
+    it('sUM: sum of two negative float with round half down and other up', () => {
       expect(calculateNumber('SUM', -2.3, -4.7)).to.equal(-7);
     });
-    it('SUM: first parameters is a string', () => {
+    it('sUM: first parameters is a string', () => {
       expect(calculateNumber('SUM', '2', 4)).to.equal(6);
     });
-    it('SUM: second parameters is a string', () => {
+    it('sUM: second parameters is a string', () => {
       expect(calculateNumber('SUM', 2, '4')).to.equal(6);
     });
-    it('SUM: both parameters are strings', () => {
+    it('sUM: both parameters are strings', () => {
       expect(calculateNumber('SUM', '2', '4')).to.equal(6);
     });
-    it('SUM: both parameters are strings, one of them in float format', () => {
+    it('sUM: both parameters are strings, one of them in float format', () => {
       expect(calculateNumber('SUM', '2', '4.6')).to.equal(7);
     });
-    it('SUM: both parameters are strings in float format', () => {
+    it('sUM: both parameters are strings in float format', () => {
       expect(calculateNumber('SUM', '2.5', '4.6')).to.equal(8);
     });
-    it('SUM: one of parameters is missing', () => {
+    it('sUM: one of parameters is missing', () => {
       expect(calculateNumber('SUM', 2)).to.be.NaN;
     });
-    it('SUM: both parameters is missing', () => {
-      expect(calculateNumber('SUM', )).to.be.NaN;
+    it('sUM: both parameters is missing', () => {
+      expect(calculateNumber('SUM')).to.be.NaN;
     });
-    it('SUM: one of parameters is NaN', () => {
+    it('sUM: one of parameters is NaN', () => {
       expect(calculateNumber('SUM', NaN, 4)).to.be.NaN;
     });
-    it('SUM: both parameters is NaN', () => {
+    it('sUM: both parameters is NaN', () => {
       expect(calculateNumber('SUM', NaN, NaN)).to.be.NaN;
     });
-    it('SUM: one of parameters is Infinity', () => {
+    it('sUM: one of parameters is Infinity', () => {
       expect(calculateNumber('SUM', Infinity, 4)).to.equal(Infinity);
     });
-    it('SUM: both parameters is Infinity', () => {
+    it('sUM: both parameters is Infinity', () => {
       expect(calculateNumber('SUM', Infinity, Infinity)).to.equal(Infinity);
     });
   });
-  describe('DIVIDE', () => {
-    it('DIVIDE: divide of two positive int', () => {
+  describe('dIVIDE', () => {
+    it('dIVIDE: divide of two positive int', () => {
       expect(calculateNumber('DIVIDE', 10, 5)).to.equal(2);
     });
-    it('DIVIDE: divide of float un integer', () => {
+    it('dIVIDE: divide of float un integer', () => {
       expect(calculateNumber('DIVIDE', 2, 4)).to.equal(0.5);
     });
-    it('DIVIDE: divide of two positive float with round half up', () => {
+    it('dIVIDE: divide of two positive float with round half up', () => {
       expect(calculateNumber('DIVIDE', 2.5, 4.6)).to.equal(0.6);
     });
-    it('DIVIDE: divide of two positive float with round half down', () => {
+    it('dIVIDE: divide of two positive float with round half down', () => {
       expect(calculateNumber('DIVIDE', 2.3, 4.4)).to.equal(0.5);
     });
-    it('DIVIDE: divide of two positive float with round half down and other up', () => {
+    it('dIVIDE: divide of two positive float with round half down and other up', () => {
       expect(calculateNumber('DIVIDE', 2.3, 4.7)).to.equal(0.4);
     });
-    it('DIVIDE: divide of integer, positive and negative', () => {
+    it('dIVIDE: divide of integer, positive and negative', () => {
       expect(calculateNumber('DIVIDE', 2, -4)).to.equal(-0.5);
     });
-    it('DIVIDE: divide of two negative int', () => {
+    it('dIVIDE: divide of two negative int', () => {
       expect(calculateNumber('DIVIDE', -2, -4)).to.equal(0.5);
     });
-    it('DIVIDE: divide of two negative float with round half up', () => {
+    it('dIVIDE: divide of two negative float with round half up', () => {
       expect(calculateNumber('DIVIDE', -2.5, -4.6)).to.equal(0.4);
     });
-    it('DIVIDE: divide of two negative float with round half down', () => {
+    it('dIVIDE: divide of two negative float with round half down', () => {
       expect(calculateNumber('DIVIDE', -2.3, -4.4)).to.equal(0.5);
     });
-    it('DIVIDE: divide of two negative float with round half down and other up', () => {
+    it('dIVIDE: divide of two negative float with round half down and other up', () => {
       expect(calculateNumber('DIVIDE', -2.3, -4.7)).to.equal(0.4);
     });
-    it('DIVIDE: first parameters is a string', () => {
+    it('dIVIDE: first parameters is a string', () => {
       expect(calculateNumber('DIVIDE', '2', 4)).to.equal(0.5);
     });
-    it('DIVIDE: second parameters is a string', () => {
+    it('dIVIDE: second parameters is a string', () => {
       expect(calculateNumber('DIVIDE', 2, '4')).to.equal(0.5);
     });
-    it('DIVIDE: both parameters are strings', () => {
+    it('dIVIDE: both parameters are strings', () => {
       expect(calculateNumber('DIVIDE', '2', '4')).to.equal(0.5);
     });
-    it('DIVIDE: both parameters are strings, one of them in float format', () => {
+    it('dIVIDE: both parameters are strings, one of them in float format', () => {
       expect(calculateNumber('DIVIDE', '2', '4.6')).to.equal(0.4);
     });
-    it('DIVIDE: both parameters are strings in float format', () => {
+    it('dIVIDE: both parameters are strings in float format', () => {
       expect(calculateNumber('DIVIDE', '2.5', '4.6')).to.equal(0.6);
     });
-    it('DIVIDE: one of parameters is missing', () => {
+    it('dIVIDE: one of parameters is missing', () => {
       expect(calculateNumber('DIVIDE', 2)).to.equal('Error');
     });
-    it('DIVIDE: both parameters is missing', () => {
-      expect(calculateNumber('DIVIDE', )).to.equal('Error');
+    it('dIVIDE: both parameters is missing', () => {
+      expect(calculateNumber('DIVIDE')).to.equal('Error');
     });
-    it('DIVIDE: one of parameters is NaN', () => {
+    it('dIVIDE: one of parameters is NaN', () => {
       expect(calculateNumber('DIVIDE', NaN, 4)).to.be.NaN;
     });
-    it('DIVIDE: both parameters is NaN', () => {
+    it('dIVIDE: both parameters is NaN', () => {
       expect(calculateNumber('DIVIDE', NaN, NaN)).to.equal('Error');
     });
-    it('DIVIDE: one of parameters is Infinity', () => {
+    it('dIVIDE: one of parameters is Infinity', () => {
       expect(calculateNumber('DIVIDE', Infinity, 4)).to.equal(Infinity);
     });
-    it('DIVIDE: both parameters is Infinity', () => {
+    it('dIVIDE: both parameters is Infinity', () => {
       expect(calculateNumber('DIVIDE', Infinity, Infinity)).to.be.NaN;
     });
-    it('DIVIDE: divide by 0', () => {
+    it('dIVIDE: divide by 0', () => {
       expect(calculateNumber('DIVIDE', 2, 0)).to.equal('Error');
     });
-    it('DIVIDE: divide by 0 with float', () => {
+    it('dIVIDE: divide by 0 with float', () => {
       expect(calculateNumber('DIVIDE', 2.5, 0)).to.equal('Error');
     });
-    it('DIVIDE: divide by 0 with negative', () => {
+    it('dIVIDE: divide by 0 with negative', () => {
       expect(calculateNumber('DIVIDE', -2, 0)).to.equal('Error');
     });
   });
