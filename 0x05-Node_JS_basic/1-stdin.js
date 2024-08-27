3a28a4d80782e4d627b2736e474a6da63262f134
Drihmia <drihmia.redouane@gmail.com>
2024-08-27 03:27:56 +0100

    refactor: Used process global object

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,9 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (name) => {
   process.stdout.write(`Your name is: ${name}`);
-  process.stdout.write('\r');
+  process.stdin.pause();
 });
 
-rl.on('close', () => {
-  console.log('This important software is now closing');
-  rl.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
