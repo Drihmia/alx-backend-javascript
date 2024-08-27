a36a8e44be56f0eb922b85cc5b13544616f9515d
Drihmia <drihmia.redouane@gmail.com>
2024-08-27 02:24:57 +0100

    fix: using .then to get the real output instead of the promise response

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,9 +2,11 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe.only('getPaymentTokenFromAPI', () => {
-  it('Async function should return True', (done) => {
-    console.log('222', getPaymentTokenFromAPI(true));
-    done();
-    expect(getPaymentTokenFromAPI(true)).to.be.true;
+  it('Async function should return True', () => {
+    getPaymentTokenFromAPI(true).then((output) => {
+      expect(output).to.deep.equal({ data: 'Successful response from the API' });
+      expect(output).to.have.property('data');
+      expect(output.data).to.equal('Successful response from the API');
+    });
   });
 });
