2c1427e956e9f794c58640aa7dd97fc5d61b806e
Drihmia <drihmia.redouane@gmail.com>
2024-08-27 02:39:17 +0100

    fix: Add done to asynchronous operations

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,11 +2,12 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe.only('getPaymentTokenFromAPI', () => {
-  it('Async function should return True', () => {
+  it('Async function should return True', (done) => {
     getPaymentTokenFromAPI(true).then((output) => {
-      expect(output).to.deep.equal({ data: 'Successful response from the API' });
       expect(output).to.have.property('data');
       expect(output.data).to.equal('Successful response from the API');
-    });
+      expect(output).to.deep.equal({ data: 'Successful response from the API' });
+      done();
+    })
   });
 });
